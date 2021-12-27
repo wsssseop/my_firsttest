7bfd1996612373497272d762efe719cd6e6ef0e4
wsssseop <wsssseop>
2021-12-27 23:01:57 +0900

    초기 커밋.

---
+++ example_EOM/example_EOM.cpp
@@ -0,0 +1,32 @@
+﻿
+
+#include <iostream>
+
+class Cents
+{
+private:
+	int m_cents;
+public:
+	Cents(int cents) { m_cents = cents; }
+	int GetCents() const
+	{
+		return m_cents;
+	}
+};
+
+Cents operator + (const Cents& c1, const Cents& c2)
+{
+	return Cents(c1.GetCents() + c2.GetCents());
+}
+
+int main()
+{
+
+	Cents c1(6);
+	Cents c2(10);
+
+
+	std::cout << (c1 + c2).GetCents() << std::endl;
+
+
+}
